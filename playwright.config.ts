c2bf41b085da32c57b859bdeb547730d93e9a4de
Liviu Mirea <liviu.mirea@wecodepixels.com>
2025-04-06 02:22:57 +0300

    Add Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,15 +43,15 @@ export default defineConfig({
 
     /* Configure projects for major browsers */
     projects: [
-        // {
-        //     name: 'chromium',
-        //     use: {...devices['Desktop Chrome']},
-        // },
-
         {
-            name: 'firefox',
-            use: {...devices['Desktop Firefox']},
+            name: 'chromium',
+            use: {...devices['Desktop Chrome']},
         },
+
+        // {
+        //     name: 'firefox',
+        //     use: {...devices['Desktop Firefox']},
+        // },
 
         // {
         //   name: 'webkit',
